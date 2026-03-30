614467329f6664a98940013b66e41bab8a58a078
Mert Türedü <62012483+MertTheTopDev@users.noreply.github.com>
2026-03-30 09:53:33 +0300

    Earth ARLearning

---
+++ EarthARLearnig/EarthARLearnig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarthARLearnig
+//
+//  Created by Mert Turedu on 29.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
