744434241fc61e8327db704e48ad51f195c328a0
Mert Türedü <62012483+MertTheTopDev@users.noreply.github.com>
2026-03-30 09:55:21 +0300

    whack a jellyfish

---
+++ Whack a jellyfish/Whack a jellyfish/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Whack a jellyfish
+//
+//  Created by Rayan Slim on 2017-08-13.
+//  Copyright © 2017 Rayan Slim. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var SceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.SceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.SceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.SceneView.addGestureRecognizer(tapGestureRecognizer)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode() {
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        jellyFishNode?.position = SCNVector3(0, 0, -1)
+        self.SceneView.scene.rootNode.addChildNode(jellyFishNode!)
+        
+
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("didn't touch anything")
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry)
+        }
+    }
+}
+
